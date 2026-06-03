5c1aa4b4342c3b381562753f5fbd3cfd65a094c7
6jbw6 <3768183086@qq.com>
2026-06-03 16:18:49 +0800

    打补丁

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from web.models.character import Character
 from web.views.create import character
+from web.views.utils.photo import remove_old_photo
 
 
 class RemoveCharacterView(APIView):
@@ -11,7 +12,10 @@ class RemoveCharacterView(APIView):
     def post(self,request):
         try:
             character_id=request.data['character_id']
-            Character.objects.filter(id=character_id,author__user=request.user).delete()
+            character=Character.objects.get(id=character_id,author__user=request.user)
+            remove_old_photo(character.photo)
+            remove_old_photo(character.background_image)
+            character.delete()
             return Response({
                 'result':'success',
             })
