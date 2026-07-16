82b32e43a26b8ed9ce9421ce4ff563ea3c1d7e45
6jbw6 <3768183086@qq.com>
2026-07-16 09:59:00 +0800

    添加系统提示词和多轮对话

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from web.models.user import UserProfile
 from web.models.character  import Character
-from web.models.friend import Friend,Message
+from web.models.friend import Friend,Message,SystemPrompt
 # Register your models here.
 
 @admin.register(UserProfile)
@@ -23,3 +23,4 @@ class MessageAdmin(admin.ModelAdmin):
     raw_id_fields=('friend',)
     
 
+admin.site.register(SystemPrompt)
